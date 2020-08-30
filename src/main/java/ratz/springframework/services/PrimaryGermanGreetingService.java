473f901e4efbac0280f3755f0eb3184e8f9713c1
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-30 01:52:11 +0000

    Added german dependency and default profile!

---
+++ src/main/java/ratz/springframework/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,18 @@
+package ratz.springframework.services;
+
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+
+
+    @Override
+    public String sayGreeting() {
+        return "Nie, kurwa, this is polak";
+    }
+}
