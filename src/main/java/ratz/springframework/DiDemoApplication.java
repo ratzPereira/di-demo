e03fdc0c555f5a42890c247c0cef0271305ed2a5
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-30 00:08:39 +0000

    simple hello demonstrating de bean dependency and application context!

--- src/main/java/ratz/springframework/DiDemoApplication.java
+++ src/main/java/ratz/springframework/DiDemoApplication.java
@@ -2,12 +2,20 @@ package ratz.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ratz.springframework.controllers.MyController;
+
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+
+        ApplicationContext ctg = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctg.getBean("myController");
+
+        controller.hello();
     }
 
 }
--- src/main/java/ratz/springframework/controllers/MyController.java
+++ src/main/java/ratz/springframework/controllers/MyController.java
@@ -1,7 +1,15 @@
 package ratz.springframework.controllers;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
 
-    
+    public String hello (){
+         System.out.println("Hello!!!!");
+
+         return "foo";
+    }
 
 }
