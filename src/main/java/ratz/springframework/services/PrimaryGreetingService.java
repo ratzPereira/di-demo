d394d4eea31243af5b91e415d2605e969901a194
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-30 02:09:58 +0000

    done
    done

---
+++ src/main/java/ratz/springframework/services/PrimaryGreetingService.java
@@ -0,0 +1,19 @@
+package ratz.springframework.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Service
+@Primary
+@Profile({"en" , "default"})
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - Primary Greeting service";
+    }
+}
