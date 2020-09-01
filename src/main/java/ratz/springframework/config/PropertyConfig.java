3c9b0d87469b245dc14ee128597507360ee77c30
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-01 14:15:18 +0000

    added environment property spring example

--- src/main/java/ratz/springframework/config/PropertyConfig.java
+++ src/main/java/ratz/springframework/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package ratz.springframework.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import ratz.springframework.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${ratz.username}")
     String user;
@@ -25,7 +30,7 @@ public class PropertyConfig {
     public FakeDataSource fakeDataSource(){
 
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
